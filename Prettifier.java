02b8190c20abfd84104252c55d18c99cc53fc70e
Yagy <hanihannes@gmail.com>
2025-03-14 15:51:35 +0200

    added condition checking for -h

---
+++ Prettifier.java
@@ -0,0 +1,7 @@
+public class Prettifier {
+    public static void main(String[] args) {
+        if (args[0].equals("-h")) {
+            System.out.println("itinerary usage:\n$ java Prettifier.java ./input.txt ./output.txt ./airport-lookup.csv");
+        }
+    }
+}
